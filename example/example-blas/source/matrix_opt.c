3a2310d2c261df01ce9ef42c2e6dc4a0787d6020
Johann Lee <me@qinka.pro>
2018-07-23 08:46:56 +0000

    update

--- example/example-blas/source/matrix_opt.c
+++ example/example-blas/source/matrix_opt.c
@@ -10,7 +10,7 @@ void f_matrix_mul2D(float *dst, float *A, float *B, int m, int n, int s) {
     #pragma omp target parallel shared(dst, A, B, m, n, s) private(i, j, k)
     #endif
     #ifdef _OACC_ENABLE_
-    #pragma acc data copyin(dst[0:m*s]), copyin(A[0:m*n], B[0:n*s]) private(i, j, k)
+    #pragma acc data copyout(dst[0:m*s]), copyin(A[0:m*n], B[0:n*s])
     #endif
     {
         int i, j, k;
@@ -22,11 +22,10 @@ void f_matrix_mul2D(float *dst, float *A, float *B, int m, int n, int s) {
         #pragma omp for
         #endif
         #ifdef _OACC_ENABLE_
-        #pragma acc kernels
+        #pragma acc parallel
         #endif
-        for(i = 0; i < m; i++)
-            for(j = 0; j < s; j++)
-                dst[i * s + j] = 0;
+        for(i = 0; i < m * s; i++)
+            dst[i] = 0;
 
         #ifdef _OMP_ENABLE_
         #pragma omp for
@@ -35,19 +34,19 @@ void f_matrix_mul2D(float *dst, float *A, float *B, int m, int n, int s) {
         #pragma omp for
         #endif
         #ifdef _OACC_ENABLE_
-        #pragma acc kernels
+        #pragma acc parallel loop
         #endif
         for (i = 0; i < m; i++)
+         #ifdef _OMP_ENABLE_
+        #pragma omp for
+        #endif
+        #ifdef _OMP_TARGET_ENABLE_
+        #pragma omp for
+          #endif
+          #ifdef _OACC_ENABLE_
+	  #pragma acc loop
+	  #endif
             for (j = 0; j < s; j++)
-                #ifdef _OMP_ENABLE_
-                #pragma omp seq
-                #endif
-                #ifdef _OMP_TARGET_ENABLE_
-                #pragma omp seq
-                #endif
-                #ifdef _OACC_ENABLE_
-                #pragma acc seq
-                #endif
                 for (k = 0; k < n; k++)
                     dst[i * s + j] += A[i * n + k] * B[k * s + j];
     }
