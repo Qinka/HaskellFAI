a052136d960a69fbf3116e7e9ff6614857ec3b6e
Johann Lee <me@qinka.pro>
2018-07-23 16:00:09 +0800

    Update [ci skip]

--- example/example-blas/source/matrix_opt.c
+++ example/example-blas/source/matrix_opt.c
@@ -2,20 +2,55 @@
 #include <config.h>
 
 void f_matrix_mul2D(float *dst, float *A, float *B, int m, int n, int s) {
-    int i, j;
-    
+
     #ifdef _OMP_ENABLE_
-    #pragma omp parallel for shared(dst, A, B, m, n, s) private(i, j)
+    #pragma omp parallel shared(dst, A, B, m, n, s) private(i, j, k)
     #endif
     #ifdef _OMP_TARGET_ENABLE_
-    #pragma omp target parallel for shared(dst, A, B, m, n, s) private(i, j)
+    #pragma omp target parallel shared(dst, A, B, m, n, s) private(i, j, k)
     #endif
-    for (i = 0; i < m; i++)
-        for (j = 0; j < s; j++) {
-            dst[i * s + j] = 0.0;
-            for (int k = 0; k < n; k++)
-                dst[i * s + j] += A[i * n + k] * B[k * s + j];
-        }
+    #ifdef _OACC_ENABLE_
+    #pragma acc data copyin(dst[0:m*s]), copyin(A[0:m*n], B[0:n*s]) private(i, j, k)
+    #endif
+    {
+        int i, j, k;
+
+        #ifdef _OMP_ENABLE_
+        #pragma omp for
+        #endif
+        #ifdef _OMP_TARGET_ENABLE_
+        #pragma omp for
+        #endif
+        #ifdef _OACC_ENABLE_
+        #pragma acc kernels
+        #endif
+        for(i = 0; i < m; i++)
+            for(j = 0; j < s; j++)
+                dst[i * s + j] = 0;
+
+        #ifdef _OMP_ENABLE_
+        #pragma omp for
+        #endif
+        #ifdef _OMP_TARGET_ENABLE_
+        #pragma omp for
+        #endif
+        #ifdef _OACC_ENABLE_
+        #pragma acc kernels
+        #endif
+        for (i = 0; i < m; i++)
+            for (j = 0; j < s; j++)
+                #ifdef _OMP_ENABLE_
+                #pragma omp seq
+                #endif
+                #ifdef _OMP_TARGET_ENABLE_
+                #pragma omp seq
+                #endif
+                #ifdef _OACC_ENABLE_
+                #pragma acc seq
+                #endif
+                for (k = 0; k < n; k++)
+                    dst[i * s + j] += A[i * n + k] * B[k * s + j];
+    }
 }
 
 void f_matrix_dot_mul2D(float *dst, float *A, float *B, int m, int n) {
