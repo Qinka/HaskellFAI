c5762cc75494749bf5cd74b4f085ce636183c895
Johann Lee <me@qinka.pro>
2018-08-06 11:32:51 +0800

    Update

--- FAI/test/Foreign/FAI/Platform/CUDASpec.hs
+++ FAI/test/Foreign/FAI/Platform/CUDASpec.hs
@@ -18,26 +18,17 @@ import           Foreign.Marshal.Array
 import           Foreign.Ptr
 import           Foreign.Storable
 
-peekBuffer :: (Storable b, Pf Host a ~ b) => Buffer Host a -> IO [b]
-peekBuffer (Buffer fp len) = withForeignPtr fp $ \p -> peekArray len p
-
-peekBufferA :: (Storable b, Pf Host a ~ b, Pf CUDA a ~ b) => Buffer CUDA a -> Accelerate CUDA [b]
-peekBufferA b = do
-  hostBuffer <- dupBufferD True b
-  liftIO $ peekBuffer hostBuffer
-
-
 spec :: Spec
 spec = do
   describe "Test CUDA" $ do
     it "copy and same" $ do
       let acc = accelerate cc $ do
             let arr1 = [1..100] :: [Float]
-                b1  = unsafeToHostBuffer arr1 :: Buffer Host Float
+                b1  = unsafeToHostBuffer arr1 :: Buffer Int Host Float
             liftIO $ print b1
-            b2 <- dupBuffer True b1 :: Accelerate CUDA (Buffer CUDA Float)
+            b2 <- dupBuffer True b1 :: Accelerate CUDA (Buffer Int CUDA Float)
             liftIO $ print b2
-            b3 <- dupBufferD True b2 :: Accelerate CUDA (Buffer Host Float)
+            b3 <- dupBufferD True b2 :: Accelerate CUDA (Buffer Int Host Float)
             liftIO $ print b3
             let arr2 = unsafePeekHostBuffer b3
             return (arr1, arr2)
