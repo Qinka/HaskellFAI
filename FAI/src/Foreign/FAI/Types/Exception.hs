7003e6e804da3387538dd5d3565ab6ff42cc27c2
Johann Lee <me@qinka.pro>
2018-08-31 10:46:17 +0800

    Checkpoint

---
+++ FAI/src/Foreign/FAI/Types/Exception.hs
@@ -0,0 +1,49 @@
+{-
+
+Copyright (C) 2018 Johann Lee <mer@qinka.pro>
+
+This fiel is part of HaskellFAI
+
+HaskellFAI is free software: you can redistribute it and/or modify
+it under the terms of the GNU Less General Public License as published by
+the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+
+Haskell is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warrenty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Less General Public License for more details.
+
+You should have received a copy of the GNU Less General Public License
+along with HaskellFAI. If not, see <http://www.gnu.org/licenses/>.
+
+-}
+
+{-|
+Module: Foreign.FAI.Types.Exception
+Description: The defined exceptions of FAI.
+Copyright: (C) 2018 Johann Lee <me@qinka.pro>
+License: LGPL3
+Maintainer: me@qinka.pro
+Stability: experimental
+Portability: unknown
+
+The defined exceptions of FAI.
+-}
+
+module Foreign.FAI.Types.Exception
+  ( module Control.Monad.Catch
+  , NullPtrAllocated(..)
+  ) where
+
+import Control.Monad.Catch
+
+-- | Buffer allocated null pointer
+data NullPtrAllocated = NullPtrAllocated
+    deriving(Eq)
+
+instance Show NullPtrAllocated where
+  show _ = "Error: The buffer allocator allocated a null pointer for the buffer."
+  
+instance Exception NullPtrAllocated
+  
